7065d5448cfe1d3f66ca9cfc7f032062c7fba7ea
Nick Kisel <insherlolqq@gmail.com>
2022-05-02 15:45:40 +0300

    add exception handler

--- src/main/java/tech/avito/BaseExceptionHandler.java
+++ src/main/java/tech/avito/BaseExceptionHandler.java
@@ -1,2 +1,33 @@
-package tech.avito;public class BaseExceptionHandler {
+package tech.avito;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
+@ResponseBody
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String noSuchElementExceptionHandler(NoSuchElementException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<String> validationExceptionHandler(MethodArgumentNotValidException exception) {
+        return exception.getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+    }
 }
