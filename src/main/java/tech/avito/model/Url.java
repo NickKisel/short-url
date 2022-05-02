a55abeada8c82a4a86fb49a92505e2424413edcf
Nick Kisel <insherlolqq@gmail.com>
2022-05-02 14:49:16 +0300

    add model/Url

---
+++ src/main/java/tech/avito/model/Url.java
@@ -0,0 +1,38 @@
+package tech.avito.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "urls")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Url {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String hash;
+
+    private String originalUrl;
+
+    private String shorterUrl;
+
+    @CreationTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date createdAt;
+}
